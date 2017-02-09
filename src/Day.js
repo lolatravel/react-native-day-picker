d674d03c407e7c170c582d3793beea5f1d260676
Amy Fairbrother <amy@lolatravel.com>
2017-02-09 11:47:43 -0500

    flexible styles for Day component

--- src/Day.js
+++ src/Day.js
@@ -8,9 +8,20 @@ import {
 }               from 'react-native';
 
 export default class Day extends React.Component {
+	_isCircle(borderRadius, borderColor){
+		if(this.props.useCircleMarkers){
+			return {
+				borderRadius: borderRadius,
+				borderWidth: 1,
+				borderColor: borderColor,
+				overflow: 'hidden'
+			}
+		}
+	}
+
 	render() {
 		let {date, status, disabled, onDayPress, width} = this.props;
-		let onPress, textColor, backColor;
+		let onPress, textColor, backColor, borderColor, borderRadius;
 
 		if (disabled) {
 			status = 'disabled';
@@ -25,28 +36,37 @@ export default class Day extends React.Component {
 			case 'disabled':
 				backColor = this.props.dayDisabledBackColor;
 				textColor = this.props.dayDisabledTextColor;
+				borderColor = 'transparent';
+				borderRadius = 0;
 				break;
 
 			case 'common':
 				backColor = this.props.dayCommonBackColor;
 				textColor = this.props.dayCommonTextColor;
+				borderColor = 'transparent';
+				borderRadius = 0;
 				break;
 
 			case 'selected':
 				backColor = this.props.daySelectedBackColor;
 				textColor = this.props.daySelectedTextColor;
+				borderColor = this.props.daySelectedBackColor;
+				borderRadius = 30;
 				break;
 
 			case 'inRange':
 				backColor = this.props.dayInRangeBackColor;
 				textColor = this.props.dayInRangeTextColor;
+				borderColor = 'transparent';
+				borderRadius = 0;
 				break;
 		}
 
 		return (
 			<TouchableOpacity
 				activeOpacity={disabled ? 1 : 0.5}
-				style={[styles.common, {backgroundColor: backColor, width: width / 7, height: width / 7}]}
+				style={[styles.common, {backgroundColor: backColor, width: width / 7, height: width / 7},
+					this._isCircle(borderRadius, borderColor)]}
 				onPress={onPress}>
 				<Text style={{color: textColor}}>{date.getDate()}</Text>
 			</TouchableOpacity>
